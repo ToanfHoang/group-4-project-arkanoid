1f2c0799528fc6a4ff725d876e0458f7ef667be7
tuanengineer <minhtuan22112006@gmail.com>
2025-10-08 23:26:01 +0700

    add GameObject new, MovableObject, Paddle

--- src/arkanoid/MovableObject.java
+++ src/arkanoid/MovableObject.java
@@ -1,4 +1,30 @@
 package arkanoid;
 
-public class MovableObject {
+import javafx.scene.canvas.GraphicsContext;
+/**
+ * lớp trừu tượng cho các đối tượng có thể di chuyển trong trò chơi
+ * kế thừa từ GameObject
+ * thêm thuộc tính dx, dy để xác định vận tốc di chuyển theo trục x và y
+ */
+public abstract class MovableObject {
+    protected double x, y, width, height;
+    protected double dx, dy;
+
+    public MovableObject(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.dx = 0;
+        this.dy = 0;
+    }
+
+    public abstract void render(GraphicsContext gc);
+    public abstract void update();
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
 }
--- src/arkanoid/Paddle.java
+++ src/arkanoid/Paddle.java
@@ -1,4 +1,37 @@
 package arkanoid;
 
-public class Paddle {
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+/*
+    * tao lớp Paddle kế thừa từ MovableObject
+    * có thể di chuyển trái phải
+    * vẽ hình chữ nhật màu xanh dương
+    * có thể dừng lại
+    * tốc độ di chuyển cố định
+    * kích thước và vị trí khởi tạo từ tham số truyền vào
+ */
+public class Paddle extends MovableObject{
+    public Paddle(double x, double y, double width, double height) {
+        super(x,y,width,height);
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.setFill(Color.BLUE);
+        gc.fillRect(x, y, width, height);
+    }
+    @Override
+    public void update(){
+        move();
+    }
+    public void moveLeft(){
+        dx = -5;
+    }
+    public void moveRight() {
+        dx = 5;
+    }
+    public void stop() {
+        dx = 0;
+    }
+
 }
